408d4f86986918d543467d7c0c76b684cf5f18f0
Siddharth230 <siddharthlokhande04@gmail.com>
2025-06-22 18:37:27 +0530

    Version 1 completed

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { DATABASE_URL, PORT } from "./config.js";
 import mongoose from "mongoose";
-import { bookModel } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -21,32 +22,8 @@ mongoose
 
 app.use(express.json());
 
+app.use(cors());
+
+app.use("/books", booksRoute);
+
 app.get("/", (req, res) => {});
-
-app.post("/books", async (req, res) => {
-  try {
-    const { title, author, publishYear } = req.body;
-
-    const book = await bookModel.create({ title, author, publishYear });
-
-    return res.send(book);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send({
-      message: err.message,
-    });
-  }
-});
-
-app.get("/books", async (req, res) => {
-  try {
-    const books = await bookModel.find({});
-
-    return res.json(books);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send({
-      message: err.message,
-    });
-  }
-});
